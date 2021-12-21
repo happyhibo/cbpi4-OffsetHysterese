21f6fd2e50d8291a76c730c34cbe0c202fceae52
HappyHibo <happyhibo@vodafone.de>
2021-12-21 08:54:55 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# craftbeerpi4 plugin OffsetHysterese
+
+...
+
+## Installation
+
+- sudo pip3 install https://github.com/
+
+## Update
+
+- sudo pip3 install --upgrade https://github.com/
+
+## Add plugin to CBPi4
+
+- sudo cbpi add cbpi4-OffsetHysterese
+
+## Remove plugin from CBPI4
+
+- sudo cbpi remove cbpi4-OffsetHysterese
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='OffsetHysterese',
+      version='0.0.1',
+      description='CraftBeerPi4 Plugin with two temp offset areas',
+      author='HappyHibo',
+      author_email='',
+      url='',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'OffsetTempHysterese': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['OffsetTempHysterese'],
+     )
